7274cb07aa9cc59dce6ad458e4c03079280070e8
Kateryna Senchenko <kateryna_senchenko@epam.com>
2018-09-11 18:29:00 +0300

    Added Validator Registry service API

---
+++ src/main/java/org/folio/validator/registry/ValidatorRegistryService.java
@@ -0,0 +1,41 @@
+package org.folio.validator.registry;
+
+import io.vertx.codegen.annotations.Fluent;
+import io.vertx.codegen.annotations.ProxyGen;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+import java.util.List;
+
+@ProxyGen
+public interface ValidatorRegistryService {
+
+  static final String VALIDATOR_REGISTRY_ADDRESS = "validator-registry.queue";
+
+  static ValidatorRegistryService create(Vertx vertx) {
+    return new ValidatorRegistryServiceImpl(vertx);
+  }
+
+  static ValidatorRegistryService createProxy(Vertx vertx, String address) {
+    return new ValidatorRegistryServiceVertxEBProxy(vertx, address);
+  }
+
+  @Fluent
+  ValidatorRegistryService getAllTenantRules(String tenantId, Handler<AsyncResult<JsonObject>> asyncResultHandler);
+
+  @Fluent
+  ValidatorRegistryService createTenantRule(String tenantId, JsonObject validationRule, Handler<AsyncResult<JsonObject>> asyncResultHandler);
+
+  @Fluent
+  ValidatorRegistryService updateTenantRule(String tenantId, JsonObject validationRule, Handler<AsyncResult<JsonObject>> asyncResultHandler);
+
+  @Fluent
+  ValidatorRegistryService getTenantRuleByRuleId(String tenantId, String ruleId, Handler<AsyncResult<JsonObject>> asyncResultHandler);
+
+  @Fluent
+  ValidatorRegistryService getActiveRulesByType(String tenantId, String type, Handler<AsyncResult<JsonObject>> asyncResultHandler);
+
+}
