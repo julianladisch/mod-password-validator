700fd7a60181b0e42a512afa44a9d54bcd4d0234
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2018-09-11 17:32:06 +0300

    initial commit with initAPI structure

---
+++ src/main/java/org/folio/rest/impl/InitAPIs.java
@@ -0,0 +1,20 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import org.folio.rest.resource.interfaces.InitAPI;
+
+/**
+ * Perform preprocessing operation before the verticle is deployed,
+ * e.g. components registration, initializing, binding.
+ *
+ * @author Igor-Gorchakov
+ */
+public class InitAPIs implements InitAPI {
+  @Override
+  public void init(Vertx vertx, Context context, Handler<AsyncResult<Boolean>> handler) {
+    // TODO Bind Vertx with new added component here
+  }
+}
