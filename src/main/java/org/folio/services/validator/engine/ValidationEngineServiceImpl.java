0b2ed4b9652d3e22c1c5c913c7377fce847c67f7
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2018-09-14 15:15:47 +0300

    Do async send password for programmatic rules endpoints

--- src/main/java/org/folio/services/validator/engine/ValidationEngineServiceImpl.java
+++ src/main/java/org/folio/services/validator/engine/ValidationEngineServiceImpl.java
@@ -1,6 +1,7 @@
 package org.folio.services.validator.engine;
 
 import io.vertx.core.AsyncResult;
+import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
@@ -12,6 +13,7 @@ import io.vertx.core.http.HttpClientRequest;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import javafx.util.Pair;
 import org.folio.rest.jaxrs.model.Rule;
 import org.folio.rest.jaxrs.model.RuleCollection;
 import org.folio.services.validator.registry.ValidatorRegistryService;
@@ -34,20 +36,30 @@ import static org.folio.rest.RestVerticle.MODULE_SPECIFIC_ARGS;
  */
 public class ValidationEngineServiceImpl implements ValidationEngineService {
 
+  // OKAPI url header with OKAPI getaway address
   private static String OKAPI_URL_HEADER = "X-Okapi-URL";
+  // OKAPI tenant header with tenant id
   private static String OKAPI_TENANT_HEADER = "X-Okapi-Tenant";
+  // OKAPI token header contains token to call OKAPI internal modules
   private static String OKAPI_TOKEN_HEADER = "X-Okapi-Token";
-  private final Logger logger = LoggerFactory.getLogger(ValidationEngineServiceImpl.class);
-  private int lookupTimeout = Integer.parseInt(MODULE_SPECIFIC_ARGS.getOrDefault("lookup.timeout", "1000"));
+  // Logger
+  private final Logger logger = LoggerFactory
+    .getLogger(ValidationEngineServiceImpl.class);
+  // Timeout to wait for response
+  private int lookupTimeout = Integer
+    .parseInt(MODULE_SPECIFIC_ARGS.getOrDefault("lookup.timeout", "1000"));
+  // Repository component to validation obtain rules
   private ValidatorRegistryService validatorRegistryProxy;
+  // Http client to call programmatic rules as internal OKAPI endpoints
   private HttpClient httpClient;
 
-  public ValidationEngineServiceImpl(Vertx vertx) {
-    this.validatorRegistryProxy = ValidatorRegistryService.createProxy(vertx, ValidatorRegistryService.ADDRESS);
+  public ValidationEngineServiceImpl(final Vertx vertx) {
+    this.validatorRegistryProxy = ValidatorRegistryService
+      .createProxy(vertx, ValidatorRegistryService.ADDRESS);
     initHttpClient(vertx);
   }
 
-  private void initHttpClient(Vertx vertx) {
+  private void initHttpClient(final Vertx vertx) {
     HttpClientOptions options = new HttpClientOptions();
     options.setConnectTimeout(lookupTimeout);
     options.setIdleTimeout(lookupTimeout);
@@ -57,7 +69,7 @@ public class ValidationEngineServiceImpl implements ValidationEngineService {
   /**
    * Validates received password.
    * Calls ValidationRegistry service to obtain rules specific to tenant,
-   * runs rules to validate password,
+   * runs rules to validate password in one loop,
    * pushes validation result into result handler to return.
    *
    * @param password       received password for validation
@@ -66,7 +78,9 @@ public class ValidationEngineServiceImpl implements ValidationEngineService {
    */
 
   @Override
-  public void validatePassword(String password, Map<String, String> requestHeaders, Handler<AsyncResult<JsonObject>> resultHandler) {
+  public void validatePassword(String password,
+                               Map<String, String> requestHeaders,
+                               Handler<AsyncResult<JsonObject>> resultHandler) {
     MultiMap caseInsensitiveHeaders = new CaseInsensitiveHeaders().addAll(requestHeaders);
     String tenantId = caseInsensitiveHeaders.get(OKAPI_TENANT_HEADER);
 
@@ -75,57 +89,101 @@ public class ValidationEngineServiceImpl implements ValidationEngineService {
 
         List<Rule> rules = response.result().mapTo(RuleCollection.class).getRules();
 
-        List<String> errorMessages = validatePasswordByRules(rules, password, caseInsensitiveHeaders);
-
-        formResponse(errorMessages, resultHandler);
+        Future<List<String>> errorMessagesFuture = validatePasswordByRules(rules, password, caseInsensitiveHeaders);
+        errorMessagesFuture.setHandler(asyncResult -> {
+          if (asyncResult.succeeded()) {
+            prepareResponse(asyncResult.result(), resultHandler);
+          } else {
+            resultHandler.handle(Future.failedFuture(asyncResult.cause().getMessage()));
+          }
+        });
       }
     });
   }
 
-  private List<String> validatePasswordByRules(List<Rule> rules, String password, MultiMap headers) {
+  private Future<List<String>> validatePasswordByRules(List<Rule> rules,
+                                                       String password,
+                                                       MultiMap headers) {
     List<String> errorMessages = new ArrayList(rules.size());
     rules.sort(Comparator.comparing(Rule::getOrderNo));
+
+    Future<List<String>> future = Future.future();
+    List<Future> programmaticRulesFutures = new ArrayList<>();
 
     for (Rule rule : rules) {
       if (Rule.Type.REG_EXP.equals(rule.getType())) {
         validatePasswordByRexExpRule(password, rule, errorMessages);
       } else if (Rule.Type.PROGRAMMATIC.equals(rule.getType())) {
-        validatePasswordByProgrammaticRule(password, rule, errorMessages, headers);
+        programmaticRulesFutures.add(getValidatePasswordByProgrammaticRuleFuture(password, rule, errorMessages, headers));
       }
     }
 
-    return errorMessages;
+    // Notify external method future handler when all programmatic rule futures complete
+    CompositeFuture.all(programmaticRulesFutures).setHandler(compositeFutureAsyncResult -> {
+      if (compositeFutureAsyncResult.succeeded()) {
+        future.complete(errorMessages);
+      } else {
+        future.fail(compositeFutureAsyncResult.cause().getMessage());
+      }
+    });
+    return future;
   }
 
-  private void validatePasswordByRexExpRule(String password, Rule rule, List<String> errorMessages) {
+  private void validatePasswordByRexExpRule(String password,
+                                            Rule rule,
+                                            List<String> errorMessages) {
     String expression = rule.getExpression();
     if (!Pattern.compile(expression).matcher(password).matches()) {
       errorMessages.add(rule.getErrMessageId());
     }
   }
 
-  private void validatePasswordByProgrammaticRule(String password, Rule rule, List<String> errorMessages, MultiMap headers) {
+  private Future<Pair<Rule, String>> getValidatePasswordByProgrammaticRuleFuture(String password,
+                                                                                 Rule rule,
+                                                                                 List<String> errorMessages,
+                                                                                 MultiMap headers) {
     String okapiURL = headers.get(OKAPI_URL_HEADER);
     String remoteModuleUrl = okapiURL + rule.getImplementationReference();
 
+    Future<Pair<Rule, String>> future = Future.future();
     HttpClientRequest passwordValidationRequest = httpClient.post(remoteModuleUrl, validationResponse -> {
-      if (validationResponse.statusCode() == 200) {
-        validationResponse.bodyHandler(body -> {
-          String validationResult = new JsonObject(body.toString()).getString("Result");
-          if ("Invalid".equals(validationResult)) {
-            errorMessages.add(rule.getErrMessageId());
+        if (validationResponse.statusCode() == 200) {
+          validationResponse.bodyHandler(body -> {
+            String validationResult = new JsonObject(body.toString()).getString("Result");
+            if ("Invalid".equals(validationResult)) {
+              errorMessages.add(rule.getErrMessageId());
+            }
+            future.complete();
+          });
+        } else {
+          // TODO Inform administrator that remote module is down
+          logger.error("Error: FOLIO module by the address " + remoteModuleUrl + " is not available.");
+          switch (rule.getValidationType()) {
+            case STRONG: {
+              String errorMessage = new StringBuilder()
+                .append("Programmatic rule ")
+                .append(rule.getName())
+                .append(" returns status code ")
+                .append(validationResponse.statusCode())
+                .toString();
+              logger.error(errorMessage);
+              future.fail(errorMessage);
+              break;
+            }
+            case SOFT: {
+              future.complete();
+              break;
+            }
+            default: {
+              String errorMessage = "Please add an action for the new added " +
+                "rule type when internal FOLIO module is not available";
+              logger.error(errorMessage);
+              future.fail(errorMessage);
+            }
           }
-        });
-      } else {
-        String errorMessage = new StringBuilder()
-          .append("Programmatic rule ")
-          .append(rule.getName())
-          .append(" returns status code ")
-          .append(validationResponse.statusCode())
-          .toString();
-        logger.error(errorMessage);
+        }
       }
-    });
+    );
     passwordValidationRequest
       .putHeader(OKAPI_TOKEN_HEADER, headers.get(OKAPI_TOKEN_HEADER))
       .putHeader(OKAPI_TENANT_HEADER, headers.get(OKAPI_TENANT_HEADER))
@@ -133,9 +191,11 @@ public class ValidationEngineServiceImpl implements ValidationEngineService {
       .putHeader("Accept", "application/json")
       .write(new JsonObject().put("password", password).toString())
       .end();
+    return future;
   }
 
-  private void formResponse(List<String> errorMessages, Handler<AsyncResult<JsonObject>> resultHandler) {
+  private void prepareResponse(final List<String> errorMessages,
+                               final Handler<AsyncResult<JsonObject>> resultHandler) {
     JsonObject validationResult = new JsonObject();
     if (errorMessages.isEmpty()) {
       validationResult.put("result", "Valid");
