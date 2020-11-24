a45431ffd1a7a1e0b0aaad36d26e3691b9c69c9b
dmtkachenko <42244720+dmtkachenko@users.noreply.github.com>
2020-11-25 01:55:24 +0200

    - converter for hashed password usages

--- mod-password-validator-server/src/main/java/org/folio/pv/client/PwnedClient.java
+++ mod-password-validator-server/src/main/java/org/folio/pv/client/PwnedClient.java
@@ -1,9 +1,13 @@
 package org.folio.pv.client;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import org.folio.pv.domain.dto.HashedPasswordUsage;
 
 @FeignClient(name = "pwned-passwords",
     url = "${pwned-passwords.client.url}",
@@ -11,6 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface PwnedClient {
 
   @GetMapping(path = "/range/{hashPrefix}", produces = MediaType.TEXT_PLAIN_VALUE)
-  String getPwdRange(@PathVariable String hashPrefix);
+  List<HashedPasswordUsage> getPwdRange(@PathVariable String hashPrefix);
 
 }
--- mod-password-validator-server/src/main/java/org/folio/pv/client/PwnedClientConfiguration.java
+++ mod-password-validator-server/src/main/java/org/folio/pv/client/PwnedClientConfiguration.java
@@ -1,17 +1,41 @@
 package org.folio.pv.client;
 
+import java.util.ArrayList;
+
 import feign.Client;
+import feign.codec.Decoder;
 import feign.okhttp.OkHttpClient;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.clientconfig.OkHttpFeignConfiguration;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
 
 @Import(OkHttpFeignConfiguration.class)
 public class PwnedClientConfiguration {
 
+  @Autowired
+  private ObjectFactory<HttpMessageConverters> messageConverters;
+
   @Bean
   public Client feignClient(okhttp3.OkHttpClient okHttpClient) {
     return new OkHttpClient(okHttpClient);
+  }
+
+  @Bean
+  public Decoder feignDecoder () {
+    var springConverters = messageConverters.getObject().getConverters();
+    var decoderConverters = new ArrayList<HttpMessageConverter<?>>(springConverters.size() + 1);
+
+    decoderConverters.addAll(springConverters);
+    decoderConverters.add(new HashedPasswordUsageCollectionConverter<>());
+
+    var httpMessageConverters = new HttpMessageConverters(decoderConverters);
+
+    return new SpringDecoder(() -> httpMessageConverters);
   }
 
 }
