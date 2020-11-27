c33a29e9ddff44023564f35bf976a2bbea089733
dmtkachenko <42244720+dmtkachenko@users.noreply.github.com>
2020-11-27 18:48:02 +0200

    - validate against PwnedPasswords service

--- mod-password-validator-server/src/main/java/org/folio/pv/service/PwnedPasswordsService.java
+++ mod-password-validator-server/src/main/java/org/folio/pv/service/PwnedPasswordsService.java
@@ -1,5 +1,8 @@
 package org.folio.pv.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -7,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import org.folio.pv.client.PwnedClient;
+import org.folio.pv.domain.dto.HashedPasswordUsage;
 import org.folio.pv.domain.dto.Password;
 
 @Service
@@ -15,16 +19,27 @@ public class PwnedPasswordsService {
 
   private final PwnedClient pwnedClient;
 
+
   public PwnedPasswordsService(@Autowired PwnedClient pwnedClient) {
     this.pwnedClient = pwnedClient;
   }
 
   public boolean validate(Password passwordContainer) {
-    var hash = DigestUtils.sha1Hex(passwordContainer.getPassword());
-    var suffix = StringUtils.left(hash, 5);
+    String hash = DigestUtils.sha1Hex(passwordContainer.getPassword()).toUpperCase();
+    String hashPrefix = StringUtils.left(hash, 5);
+    String hashSuffix = hash.substring(5);
 
-    var response = pwnedClient.getPwdRange(suffix);
+    log.debug("Checking password with prefix: {}", hashPrefix);
 
-    return !response.isEmpty();
+    List<HashedPasswordUsage> usages = pwnedClient.getPwdRange(hashPrefix);
+
+    Optional<HashedPasswordUsage> knownHash = usages.stream()
+        .filter(usage -> usage.getSuffix().equals(hashSuffix) && usage.getUsageCount() > 0)
+        .findFirst();
+
+    log.info("Pwned Passwords validation: usageCount = {}",
+        knownHash.map(HashedPasswordUsage::getUsageCount).orElse(0));
+
+    return knownHash.isEmpty();
   }
 }
