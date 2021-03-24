507571bb6b19818680fc7db21d110704d41ac608
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2021-03-24 12:38:58 +0200

    MODPWD-59 Add admin health-check endpoint (#71)
    * MODPWD-59 Add admin health-check endpoint
    
    * remove slash
    
    * modify health check url
    
    * modify health check url
    
    * test modified pipeline
    
    * revert test pipeline branch back to master
    
    * Add changes to make health check working even though DB is not accessible
    
    - add configuration settings to start up application despite of any DB connection issues
    - disable all default health indicators but enable Readiness indicator
    - assign default fake values to DB related variable in run-env.sh
    
    * Increase memory settings in ModuleDescriptor-template.json
    
    Co-authored-by: John Malconian <malc@indexdata.com>

--- run-env.sh
+++ run-env.sh
@@ -4,8 +4,14 @@
 #touch ${DB_PROPS_FILE}
 #echo "spring.datasource.username=${DB_USERNAME} spring.datasource.password=${DB_PASSWORD} spring.datasource.url=jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}" | sed 's/\s\+/\n/g' | tee ${DB_PROPS_FILE}
 #
-DB_URL="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
+
+############################################################################
+# Assign fake values to variables (like DB_HOST/DB_PORT etc) if not provided
+# This way DB connection won't be established but the application boots up
+############################################################################
+
+DB_URL="jdbc:postgresql://${DB_HOST:-localhost}:${DB_PORT:-5432}/${DB_DATABASE:-db}"
 #
-DB_OPTS="-Dspring.datasource.username=${DB_USERNAME} -Dspring.datasource.password=${DB_PASSWORD} -Dspring.datasource.url=${DB_URL}"
+DB_OPTS="-Dspring.datasource.username=${DB_USERNAME:-user} -Dspring.datasource.password=${DB_PASSWORD:-pass} -Dspring.datasource.url=${DB_URL}"
 #
 export JAVA_OPTIONS="${JAVA_OPTIONS:-} ${DB_OPTS}"
